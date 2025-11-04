c55fbd1dd828c2cbfb9c0395726735856c4ba58a
Mr_shamlo <dorevgi4@gmail.com>
2025-11-04 08:40:06 +0200

    refactor name

---
+++ src/algorithms/SearchVertex.java
@@ -0,0 +1,12 @@
+package algorithms;
+
+import components.Vertex;
+
+public class SearchVertex<T> extends Vertex<T> {
+
+	public SearchVertex(T label) {
+		super(label);
+		// TODO Auto-generated constructor stub
+	}
+
+}
